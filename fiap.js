a8a0d30c742cff76f30b6bb8a4f730ac7c1c35e3
Bruno Gama <106169955+brunogama18@users.noreply.github.com>
2024-06-25 17:57:58 -0300

    Projeto Finalizado

--- fiap.js
+++ fiap.js
@@ -9,6 +9,7 @@ function handleScroll() {
 }
 window.addEventListener('scroll', handleScroll);
 
+//Barra Vermelha na Navbar//
 function updateProgressBar() {
     const { scrollTop, scrollHeight } = document.documentElement;
     const scrollPercent = scrollTop / (scrollHeight - window.innerHeight) * 100 + '%';
@@ -44,4 +45,37 @@ document.addEventListener('DOMContentLoaded', () => {
           }
         }
     );
+});
+
+
+
+
+//animação da agua//
+document.addEventListener('scroll', function () {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+    const scrollFraction = scrollTop / (scrollHeight - clientHeight);
+    const frameIndex = Math.min(
+        190, // Max frame index (191 images, 0-indexed)
+        Math.floor(scrollFraction * 191)
+    );
+
+    const imgElement = document.getElementById('waterImage');
+    imgElement.src = `assets/imgs/water/water_${String(frameIndex + 50).padStart(3, '0')}.jpg`;
+});
+
+
+
+//animação hr em cima da tecnologoia - seção cursos//
+document.addEventListener('DOMContentLoaded', function() {
+    const items = document.querySelectorAll('.item');
+
+    items.forEach(item => {
+        item.addEventListener('click', function() {
+            // Remove a classe 'active' de todos os itens
+            items.forEach(i => i.classList.remove('active'));
+            
+            // Adiciona a classe 'active' ao item clicado
+            this.classList.add('active');
+        });
+    });
 });
